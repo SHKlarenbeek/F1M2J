3c4e0e0678bd22fffa4cbf2f5891d712f8918396
SHKlarenbeek <34334@ma-web.nl>
2021-11-30 12:21:13 +0100

    Werkende Koelkast

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,22 @@
 console.log("Script is geladen!")
+
+const groentevak = document.getElementById('groente')
+const fruitvak = document.getElementById('fruit')
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'navy';
+    fruitvak.appendChild(nieuwElement);
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuweElement = document.createElement('h2');
+    nieuweElement.innerHTML = groente;
+    nieuweElement.className = 'navy';
+    groentevak.appendChild(nieuweElement);
+}
